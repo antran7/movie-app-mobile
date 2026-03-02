f61f27ec2a90791f14d2b54a4c69ef90b8dfafcb
tranthienan <tranthienan062004@gmail.com>
2026-03-02 14:19:31 +0700

    feat(ui): design movies and tv/series screens

--- app/(tabs)/movies.tsx
+++ app/(tabs)/movies.tsx
@@ -1,14 +1,59 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React from "react";
+import Colors from "@/constants/colors";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { movies } from "@/mock-data";
+import MovieCard from "@/components/MovieCard";
+import FilterItem from "@/components/FilterItem";
 
 const MovieScreen = () => {
   return (
-    <View>
-      <Text>MovieScreen</Text>
+    <View style={styles.container}>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          marginVertical: 20,
+        }}
+      >
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          <FilterItem title="Category" />
+          <FilterItem title="Region" />
+          <FilterItem title="Release Year" />
+        </ScrollView>
+
+        <TouchableOpacity activeOpacity={0.8} style={{ padding: 10 }}>
+          <MaterialCommunityIcons
+            name="filter-outline"
+            size={24}
+            color={Colors.text}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <FlatList
+        data={movies}
+        renderItem={({ item }) => (
+          <MovieCard genre={item.genre} title={item.title} image={item.image} />
+        )}
+        numColumns={3}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default MovieScreen
+export default MovieScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.background,
+  },
+});
---
+++ components/FilterItem.tsx
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import Colors from "@/constants/colors";
+import { Entypo } from "@expo/vector-icons";
+
+const FilterItem = ({ title }: { title: string }) => {
+  return (
+    <TouchableOpacity activeOpacity={0.8} style={styles.categorywrapper}>
+      <Text style={{ color: Colors.text, marginRight: 6, fontSize: 12 }}>
+        {title}
+      </Text>
+      <Entypo name="chevron-small-down" size={20} color={Colors.text} />
+    </TouchableOpacity>
+  );
+};
+
+export default FilterItem;
+
+const styles = StyleSheet.create({
+  categorywrapper: {
+    borderWidth: 1,
+    borderColor: Colors.gray,
+    padding: 8,
+    borderRadius: 18,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginRight: 14,
+  },
+});
